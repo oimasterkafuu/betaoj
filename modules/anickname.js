baafacd088e52a2b2680f916be49808fb672c05a
oimasterkafuu <oimasterfake@icloud.com>
2023-07-05 18:14:42 +0800

    Fix: anickname

--- modules/anickname.js
+++ modules/anickname.js
@@ -1,70 +1,5 @@
 let User = syzoj.model('user');
 
-app.get('*', async (req, res, next) => {
-    try {
-        if (res.locals.user && !res.locals.user.nickname)
-            res.render('ticket');
-        else
-            next();
-    } catch (e) {
-        syzoj.log(e);
-        res.render('error', {
-            err: e
-        })
-    }
-});
-app.post('*', async (req, res, next) => {
-    try {
-        if (res.locals.user && !res.locals.user.nickname)
-            res.render('ticket');
-        else
-            next();
-    } catch (e) {
-        syzoj.log(e);
-        res.render('error', {
-            err: e
-        })
-    }
-});
-app.get('*', async (req, res, next) => {
-    try {
-        if (!res.locals.user || !res.locals.user.is_admin){
-            next();
-            return;
-        }
-        let users = (await User.find({
-            where: [
-                { permission: null }
-            ]
-        })).filter(user => user.nickname && !user.username.startsWith('bannedUser'));
-        
-        res.locals.needPass = users.length;
-        next();
-    } catch (e) {
-        syzoj.log(e);
-        next();
-    }
-});
-app.post('*', async (req, res, next) => {
-    try {
-        if (!res.locals.user || !res.locals.user.is_admin){
-            next();
-            return;
-        }
-        let users = (await User.find({
-            where: [
-                { permission: null }
-            ]
-        })).filter(user => user.nickname && !user.username.startsWith('bannedUser'));
-        
-        res.locals.needPass = users.length;
-        next();
-    } catch (e) {
-        syzoj.log(e);
-        next();
-    }
-});
-
 app.post('/anickname', async (req, res) => {
     try {
         if (!res.locals.user)
@@ -100,5 +35,70 @@ app.get('/teach', async (req, res) => {
         res.render('error', {
             err: e
         })
+    }
+});
+
+app.get('*', async (req, res, next) => {
+    try {
+        if (res.locals.user && !res.locals.user.nickname)
+            res.render('ticket');
+        else
+            next();
+    } catch (e) {
+        syzoj.log(e);
+        res.render('error', {
+            err: e
+        })
+    }
+});
+app.post('*', async (req, res, next) => {
+    try {
+        if (res.locals.user && !res.locals.user.nickname)
+            res.render('ticket');
+        else
+            next();
+    } catch (e) {
+        syzoj.log(e);
+        res.render('error', {
+            err: e
+        })
+    }
+});
+app.get('*', async (req, res, next) => {
+    try {
+        if (!res.locals.user || !res.locals.user.is_admin){
+            next();
+            return;
+        }
+        let users = (await User.find({
+            where: [
+                { permission: null }
+            ]
+        })).filter(user => user.nickname && !user.username.startsWith('bannedUser'));
+        
+        res.locals.needPass = users.length;
+        next();
+    } catch (e) {
+        syzoj.log(e);
+        next();
+    }
+});
+app.post('*', async (req, res, next) => {
+    try {
+        if (!res.locals.user || !res.locals.user.is_admin){
+            next();
+            return;
+        }
+        let users = (await User.find({
+            where: [
+                { permission: null }
+            ]
+        })).filter(user => user.nickname && !user.username.startsWith('bannedUser'));
+        
+        res.locals.needPass = users.length;
+        next();
+    } catch (e) {
+        syzoj.log(e);
+        next();
     }
 });
