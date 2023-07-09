6a5e42a08b8b679e87f5d5bc2f154bf06bac8087
oimasterkafuu <oimasterfake@icloud.com>
2023-07-09 12:53:28 +0800

    avoid 'new'

--- modules/index.js
+++ modules/index.js
@@ -29,7 +29,7 @@ app.get('/', async (req, res) => {
             title: article.title,
             url: syzoj.utils.makeUrl(['article', article.id]),
             date: syzoj.utils.formatDate(article.public_time, 'L'),
-            new: (syzoj.utils.getCurrentDate() - article.public_time) <= 7 * 24 * 60 * 60 * 1000
+            isNew: (syzoj.utils.getCurrentDate() - article.public_time) <= 7 * 24 * 60 * 60
         }));
 
         let fortune = null;
